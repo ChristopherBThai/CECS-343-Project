2fcd6b94665bb6178977956bedfd8c63813ee61d
Aingty Eung <aingtyeung@yahoo.com>
2017-10-11 15:56:04 -0700

    Finish Database Creation SQL No insert data though, Still need to do that.

--- eHandy-Database.sql
+++ eHandy-Database.sql
@@ -13,9 +13,9 @@ CREATE TABLE Business
 (
     bName       VARCHAR(20)     NOT NULL,
     webID       INT             NOT NULL,
+    bEmail      VARCHAR(20)     NOT NULL,
     bAddress    VARCHAR(60),
     bPhoneNum   VARCHAR(10),
-    bEmail      VARCHAR(20),
     CONSTRAINT  Business_PK PRIMARY KEY(bName, webID)
 );
 
@@ -23,8 +23,20 @@ CREATE TABLE Home_Service
 (
     serviceNeed VARCHAR(15)     NOT NULL,
     hName       VARCHAR(20)     NOT NULL,
-    serviceDate DATETIME,
+    serviceDate DATE            NOT NULL,
+    serviceLoc  VARCHAR(30),
     CONSTRAINT  Home_Service_FK FOREIGN KEY(hName) REFERENCES Homeowner.hName,
     CONSTRAINT  Home_Service_PK PRIMARY KEY(serviceNeed)
 );
+
+CREATE TABLE Business_Service
+(
+    serviceType VARCHAR(15)     NOT NULL,
+    bName       VARCHAR(20)     NOT NULL,
+    serviceRate FLOAT          NOT NULL,
+    startDate   DATE,
+    CONSTRAINT  Business_Service_FK FOREIGN KEY(bName) REFERENCES Business.bName,
+    CONSTRAINT  Business_Service_PK PRIMARY KEY(serviceType, serviceRate)
+);
+
 
