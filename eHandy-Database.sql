fc7206605ca102f954d6c36ee241c289d1c62bfd
Kunaledge <kunal1005@yahoo.com>
2017-10-11 19:53:40 -0700

    Corrected SQL Tables with PK and FK

--- eHandy-Database.sql
+++ eHandy-Database.sql
@@ -5,9 +5,12 @@ CREATE TABLE Homeowner
     hAddress    VARCHAR(60),
     hPhoneNum   VARCHAR(10),
     hEmail      VARCHAR(20),
-    houseType   VARCHAR(30),
-    CONSTRAINT  Homeowner_PK PRIMARY KEY(hName, webID) 
+    houseType   VARCHAR(30)
+--     CONSTRAINT  Homeowner_PK PRIMARY KEY(hName, webID) 
 );
+
+ALTER TABLE Homeowner
+    ADD CONSTRAINT Homeowner_PK PRIMARY KEY (hName,webID);
 
 CREATE TABLE Business
 (
@@ -15,28 +18,38 @@ CREATE TABLE Business
     webID       INT             NOT NULL,
     bEmail      VARCHAR(20)     NOT NULL,
     bAddress    VARCHAR(60),
-    bPhoneNum   VARCHAR(10),
-    CONSTRAINT  Business_PK PRIMARY KEY(bName, webID)
+    bPhoneNum   VARCHAR(10)
+--     CONSTRAINT  Business_PK PRIMARY KEY(bName, webID)
 );
+
+ALTER TABLE Business
+    ADD CONSTRAINT Business_PK PRIMARY KEY (bName,webID);
+
 
 CREATE TABLE Home_Service
 (
     serviceNeed VARCHAR(15)     NOT NULL,
     hName       VARCHAR(20)     NOT NULL,
+    web1ID      INT             NOT NULL,
     serviceDate DATE            NOT NULL,
     serviceLoc  VARCHAR(30),
-    CONSTRAINT  Home_Service_FK FOREIGN KEY(hName) REFERENCES Homeowner.hName,
     CONSTRAINT  Home_Service_PK PRIMARY KEY(serviceNeed)
 );
+
+ALTER TABLE Home_Service   
+    ADD CONSTRAINT  Home_Service_FK FOREIGN KEY(hName,web1ID) REFERENCES Homeowner (hName, webID);
+
 
 CREATE TABLE Business_Service
 (
     serviceType VARCHAR(15)     NOT NULL,
-    bName       VARCHAR(20)     NOT NULL,
-    serviceRate FLOAT          NOT NULL,
+    b1Name      VARCHAR(20)     NOT NULL,
+    web1ID      INT             NOT NULL,
+    serviceRate FLOAT           NOT NULL,
     startDate   DATE,
-    CONSTRAINT  Business_Service_FK FOREIGN KEY(bName) REFERENCES Business.bName,
     CONSTRAINT  Business_Service_PK PRIMARY KEY(serviceType, serviceRate)
+   
 );
 
-
+ALTER TABLE Business_Service
+    ADD CONSTRAINT  Business_Service_FK FOREIGN KEY (b1Name,web1ID) REFERENCES Business (bName,webID);
