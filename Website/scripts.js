e4b57b7c85466428332b5a587a942ee553965513
ChristopherBThai <christopherbthai@gmail.com>
2017-10-17 15:33:21 -0700

    Added groups, boxes and classes to display the results from the search

--- Website/scripts.js
+++ Website/scripts.js
@@ -11,25 +11,76 @@ function createText() {
 
 function search(ele){
     if(event.keyCode==13){
-        createGroupTitle(ele.value);
-        createGroupLine();
+		var group = new Group(ele.value,new Array(0));
+		group.list.push(new Result("TestName1","TestPhone","TestDesc"));
+		group.list.push(new Result("TestName2","TestPhone","TestDesc"));
+		group.list.push(new Result("TestName3","TestPhone","TestDesc"));
+        createGroupTitle(group.name);
+        createGroupLine(group.name);
+		createResult(group.name,group.list[0]);
+		createResult(group.name,group.list[1]);
+		createResult(group.name,group.list[2]);
     }
 }
 
-function createGroupTitle(name){
-    var resultDisplay = document.getElementById("display");
-    var div = document.createElement('div');
-    div.setAttribute('class','result-title');
-    var para = document.createElement("a");
-    var text = document.createTextNode(name);
-    para.appendChild(text);
-    div.appendChild(para);
-    resultDisplay.appendChild(div);
+class Group {
+	constructor(name,list){
+		this.list = list;
+		this.name = name;
+	}
 }
 
-function createGroupLine(){
-    var resultDisplay = document.getElementById("display");
-    var div = document.createElement('div');
-    div.setAttribute('class','result-line');
-    resultDisplay.appendChild(div);
+class Result {
+	constructor(name,phone,desc){
+		this.name = name;
+		this.phone = phone;
+		this.desc = desc;
+	}
+}
+
+function createGroupTitle(name){
+	if(document.getElementById(name)==null){
+		var resultDisplay = document.getElementById("display");
+
+		var divOuter = document.createElement('div');
+		divOuter.setAttribute('id',name);
+
+		var div = document.createElement('div');
+		div.setAttribute('class','result-title');
+		var para = document.createElement("a");
+		var text = document.createTextNode(name);
+
+		para.appendChild(text);
+		div.appendChild(para);
+		divOuter.appendChild(div);
+		resultDisplay.appendChild(divOuter);
+	}
+}
+
+function createGroupLine(name){
+	if(document.getElementById("line"+name)==null){
+    	var resultDisplay = document.getElementById(name);
+    	var div = document.createElement('div');
+   		div.setAttribute('class','result-line');
+		div.setAttribute('id',"line"+name);
+    	resultDisplay.appendChild(div);
+	}
+}
+
+function createResult(name,result){
+	var div = document.getElementById("buttons"+name);
+	if(div==null){
+		var resultDisplay = document.getElementById(name);
+		var divOuter = document.createElement('div');
+		divOuter.setAttribute('class',"result-box-padding");
+		div = document.createElement('div');
+		div.setAttribute('class',"result-box");
+		div.setAttribute('id',"buttons"+name);
+		resultDisplay.appendChild(divOuter);
+		divOuter.appendChild(div);
+	}
+	var button = document.createElement('button');
+	var text = document.createTextNode(result.name);
+	button.appendChild(text);
+	div.appendChild(button);
 }
