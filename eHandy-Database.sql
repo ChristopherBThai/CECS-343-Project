8a87a4dac4c1513cd9f133e36648acf4bdbc21b2
Aingty Eung <aingtyeung@yahoo.com>
2017-10-18 16:54:23 -0700

    Updated on Insertion

--- eHandy-Database.sql
+++ eHandy-Database.sql
@@ -49,3 +49,141 @@ CREATE TABLE Review
     CONSTRAINT  Reieve_PK PRIMARY KEY(dateReview, reviewStars)  
 );
 
+
+-- Useful Commands --
+SELECT 
+    hName AS "Full Name", 
+    hWebID AS "ID", 
+    hEmail AS "Email", 
+    hAddress AS "Address", 
+    hPhoneNum AS "Phone Number" 
+FROM Homeowner ORDER BY hName;
+
+SELECT 
+    bName AS "Full Name", 
+    bWebID AS "ID", 
+    bEmail AS "Email", 
+    bAddress AS "Address", 
+    bPhoneNum AS "Phone Number" 
+FROM Business ORDER BY bName;
+
+SELECT 
+    typeName AS "Home Type", 
+    hWebID AS "ID", 
+    monthlyPay AS "Monthly Cost"
+FROM HouseType ORDER BY typeName;
+
+SELECT 
+    typeName AS "Business Type", 
+    bWebID AS "ID", 
+    typeRate AS "Cost Per Hour"
+FROM BusinessType ORDER BY typeName;
+
+
+
+
+
+-- Insertion Section for Homeowner --
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Aingty Eung',
+    2772,
+    'aingtyeung@yahoo.com',
+    '1234 Avenue St. Long Beach, CA 90807',
+    '5627357936'
+);
+----
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Brian Pham',
+    4805,
+    'brianthongpham@gmail.com',
+    '1234 Broadway St. Mordor, CA 91122',
+    '6572076886'
+);
+----
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Christopher Thai',
+    2962,
+    'ChristopherBThai@gmail.com',
+    '239 Temp St. LAS VegAS, NV 89834',
+    '7146249903'
+);
+----
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Kunal Patel',
+    9054,
+    'kunal1005@yahoo.com',
+    '9876 Ocean Dr. Dragonstone, NY 73451',
+    '5623087579'
+);
+----
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Matthew Vu',
+    5195,
+    'tokamakuyokuu@gmail.com',
+    '4971 High Road Mustafar, TX 28743',
+    '6576660273'
+);
+-- End of Homeowner Insertion --
+
+-- Insertion Section for Business --
+INSERT INTO Business
+(
+    bName, 
+    bWebID, 
+    bEmail, 
+    bAddress, 
+    bPhoneNum
+)
+VALUES
+(
+    'Amazon',
+    1111,
+    'Amazon@amazon.com',
+    '3278 Olive Ave. Gotham, WA 72830',
+    '8105738268'
+);
