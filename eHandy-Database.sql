d1042ee0feb294f31db0157e80ec8eb66ea74ebe
bpham <brianthongpham@gmail.com>
2017-10-19 08:34:15 -0700

    code is reorganized and cleaned

--- eHandy-Database.sql
+++ eHandy-Database.sql
@@ -84,50 +84,12 @@ FROM BusinessType ORDER BY typeName;
 
 
 -- Insertion Section for Homeowner --
-INSERT INTO Homeowner
-(
-    hName, 
-    hWebID, 
-    hEmail, 
-    hAddress, 
-    hPhoneNum
-)
-VALUES
-(
-    'Aingty Eung',
-    2772,
-    'aingtyeung@yahoo.com',
-    '1234 Avenue St. Long Beach, CA 90807',
-    '5627357936'
-),
-(
-    'Brian Pham',
-    4805,
-    'brianthongpham@gmail.com',
-    '1234 Broadway St. Mordor, CA 91122',
-    '6572076886'
-),
-(
-    'Christopher Thai',
-    2962,
-    'ChristopherBThai@gmail.com',
-    '239 Temp St. LAS VegAS, NV 89834',
-    '7146249903'
-),
-(
-    'Kunal Patel',
-    9054,
-    'kunal1005@yahoo.com',
-    '9876 Ocean Dr. Dragonstone, NY 73451',
-    '5623087579'
-),
-(
-    'Matthew Vu',
-    5195,
-    'tokamakuyokuu@gmail.com',
-    '4971 High Road Mustafar, TX 28743',
-    '6576660273'
-);
+INSERT INTO Homeowner(hName, hWebID, hEmail, hAddress, hPhoneNum) VALUES
+('Aingty Eung',2772,'aingtyeung@yahoo.com','1234 Avenue St. Long Beach, CA 90807','5627357936'),
+('Brian Pham',4805,'brianthongpham@gmail.com','1234 Broadway St. Mordor, CA 91122','6572076886'),
+('Christopher Thai',2962,'ChristopherBThai@gmail.com','239 Temp St. LAS VegAS, NV 89834','7146249903'),
+('Kunal Patel',9054,'kunal1005@yahoo.com','9876 Ocean Dr. Dragonstone, NY 73451','5623087579'),
+('Matthew Vu',5195,'tokamakuyokuu@gmail.com','4971 High Road Mustafar, TX 28743','6576660273');
 -- End of Homeowner Insertion --
 
 -- Insertion Section for Business --
