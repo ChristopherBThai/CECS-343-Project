f90100e4e2892cf3ced3b8c70d0f5dc2d430c2f8
bpham <brianthongpham@gmail.com>
2017-10-18 17:21:29 -0700

    more inserts to sql

--- eHandy-Database.sql
+++ eHandy-Database.sql
@@ -42,15 +42,152 @@ CREATE TABLE Review
     reviewStars INT             NOT NULL,
     hWebID      INT             NOT NULL,
     bWebID      INT             NOT NULL,
+    rDetails    VARCHAR(500),
     rAnonymous  VARCHAR(1),
     CONSTRAINT  Review_Home_FK FOREIGN KEY(hWebID) REFERENCES Homeowner(hWebID),
     CONSTRAINT  Review_Business_FK FOREIGN KEY(bWebID) REFERENCES Business(bWebID),
     CONSTRAINT  Reieve_PK PRIMARY KEY(dateReview, reviewStars)  
 );
 
-<<<<<<< HEAD
-ALTER TABLE Business_Service
-    ADD CONSTRAINT  Business_Service_FK FOREIGN KEY (b1Name,web1ID) REFERENCES Business (bName,webID);
-=======
-##### INSERTS HERE!!!! #####
->>>>>>> 6ed573b98877e35b78b088dd8bf46f677f2e9dd8
+
+-- Useful Commands --
+SELECT 
+    hName AS "Full Name", 
+    hWebID AS "ID", 
+    hEmail AS "Email", 
+    hAddress AS "Address", 
+    hPhoneNum AS "Phone Number" 
+FROM Homeowner ORDER BY hName;
+
+SELECT 
+    bName AS "Full Name", 
+    bWebID AS "ID", 
+    bEmail AS "Email", 
+    bAddress AS "Address", 
+    bPhoneNum AS "Phone Number" 
+FROM Business ORDER BY bName;
+
+SELECT 
+    typeName AS "Home Type", 
+    hWebID AS "ID", 
+    monthlyPay AS "Monthly Cost"
+FROM HouseType ORDER BY typeName;
+
+SELECT 
+    typeName AS "Business Type", 
+    bWebID AS "ID", 
+    typeRate AS "Cost Per Hour"
+FROM BusinessType ORDER BY typeName;
+
+
+
+
+
+-- Insertion Section for Homeowner --
+INSERT INTO Homeowner
+(
+    hName, 
+    hWebID, 
+    hEmail, 
+    hAddress, 
+    hPhoneNum
+)
+VALUES
+(
+    'Aingty Eung',
+    2772,
+    'aingtyeung@yahoo.com',
+    '1234 Avenue St. Long Beach, CA 90807',
+    '5627357936'
+),
+(
+    'Brian Pham',
+    4805,
+    'brianthongpham@gmail.com',
+    '1234 Broadway St. Mordor, CA 91122',
+    '6572076886'
+),
+(
+    'Christopher Thai',
+    2962,
+    'ChristopherBThai@gmail.com',
+    '239 Temp St. LAS VegAS, NV 89834',
+    '7146249903'
+),
+(
+    'Kunal Patel',
+    9054,
+    'kunal1005@yahoo.com',
+    '9876 Ocean Dr. Dragonstone, NY 73451',
+    '5623087579'
+),
+(
+    'Matthew Vu',
+    5195,
+    'tokamakuyokuu@gmail.com',
+    '4971 High Road Mustafar, TX 28743',
+    '6576660273'
+);
+-- End of Homeowner Insertion --
+
+-- Insertion Section for Business --
+INSERT INTO Business(bName, bWebID, bEmail, bAddress, bPhoneNum) VALUES
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+('Amazon',1111,'Amazon@amazon.com','3278 Olive Ave. Gotham, WA 72830','8105738268')
+;
