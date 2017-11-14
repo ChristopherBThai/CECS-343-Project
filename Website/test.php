734699610fb7892a2142bfe1519f339591e3f636
ChristopherBThai <christopherbthai@gmail.com>
2017-11-13 17:29:56 -0800

    Updated php

--- Website/test.php
+++ Website/test.php
@@ -1,27 +1,42 @@
 <?php
+
+	echo '<script>console.log("Starting php")</script>';
+
 	$servername="localhost";
 	$username="ehandy";
 	$password="jayden";
 	$dbname="eHandy";
 
+	echo '<script>CONSOLE.Log("Starting connection")</script>';
+
 	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	echo '<script>console.log("Checking connection")</script>';
 	if($conn->connect_error){
+		echo "failed";
+		echo '<script>console.log("Connection failed")</script>';
 		die("Connection failed: ".$conn->connect_error);
 	}
 	
-	echo "Connected successfully\n";
+	echo "success!";
+	echo '<script>console.log("Connection success!")</script>';
 
-	$sql = "SELECT * from Homeowner";
+	$sql = "SELECT * from Business";
 	$result = $conn->query($sql);
 
 	if($result->num_rows>0){
 		while($row = $result->fetch_assoc()){
-			echo $row["hName"]."\n";
+			echo '<script>console.log("'.$row["bName"].'")</script>';
 		}
 	}else{
-		echo "0 results";
+		echo '<script>console.log("0 results")</script>';
 	}
 
-	$conn->close();
-
 ?>
+
+<html>
+	<head></head>
+	<body>
+		<a>SQL Test</a>
+	</body>
+</html>
