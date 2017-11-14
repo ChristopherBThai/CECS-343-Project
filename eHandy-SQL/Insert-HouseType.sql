c9001e98b07e8e3ac57e064d41f384ec57ecf529
bpham <brianthongpham@gmail.com>
2017-11-14 08:42:54 -0800

    Added a file insert_HouseType.sql

---
+++ eHandy-SQL/Insert-HouseType.sql
@@ -0,0 +1,8 @@
+-- Insertion Section for Homeowner --
+INSERT INTO Homeowner(typeName, hWebID) VALUES
+('Igloo',2772),
+('Standard House',4805),
+('Condo',2962),
+('Mansion',9054),
+('Apartment',5195);
+-- End of Homeowner Insertion --
