20af90c48a495f95138a2208a01378b821a6ec30
ChristopherBThai <christopherbthai@gmail.com>
2017-12-11 16:14:11 -0800

    Added working review stars

--- Website/index.php
+++ Website/index.php
@@ -106,20 +106,22 @@
 				<p><b>E-mail: </b><a id="infoEmail" href="#" >example@gmail.com</a></p>
             </div> 
             <div class="container" style="background-color:#f1f1f1">
-		<textarea type="text" id="reviewtext" class="reviewtext" style="resize:none;"></textarea>
-		<div id="ratingsForm">
-			<div class="stars">
-				<input type="radio" name="star" class="star-1" id="star-1" />
-				<label class="star-1" for="star-1">1</label>
-				<input type="radio" name="star" class="star-2" id="star-2" />
-				<label class="star-2" for="star-2">2</label>
-				<input type="radio" name="star" class="star-3" id="star-3" />
-				<label class="star-3" for="star-3">3</label>
-				<input type="radio" name="star" class="star-4" id="star-4" />
-				<label class="star-4" for="star-4">4</label>
-				<input type="radio" name="star" class="star-5" id="star-5" />
-				<label class="star-5" for="star-5">5</label>
-				<span></span>
+		<div id="reviewtext" class="reviewtext">
+			<textarea type="text" id="reviewtextinput" style="resize:none;"></textarea>
+			<div id="ratingsForm" class="starrating">
+				<div class="stars">
+					<input type="radio" name="star" class="star-1" id="star-1" />
+					<label class="star-1" for="star-1">1</label>
+					<input type="radio" name="star" class="star-2" id="star-2" />
+					<label class="star-2" for="star-2">2</label>
+					<input type="radio" name="star" class="star-3" id="star-3" />
+					<label class="star-3" for="star-3">3</label>
+					<input type="radio" name="star" class="star-4" id="star-4" />
+					<label class="star-4" for="star-4">4</label>
+					<input type="radio" name="star" class="star-5" id="star-5" />
+					<label class="star-5" for="star-5">5</label>
+					<span></span>
+				</div>
 			</div>
 		</div>
                 <button id="reviewbutton" class="formButton reviewbtn" type="button" onClick="createReviewBox()">Write Review</button>
